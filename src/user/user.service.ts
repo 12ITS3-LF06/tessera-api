2f9226f37a545afb4a72d35f397353fa996eb626
Max Schäfer <max-schaefer-lg@gmx.de>
2021-10-02 12:53:06 +0200

    fix: decorate UserService with Injectable

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,9 @@
 import { CommandBus } from '@nestjs/cqrs';
 import { RegisterUserDto } from './user.dto';
 import { RegisterUserCommand } from './commands/impl/register-user.command';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class UserService {
   constructor(private readonly commandBus: CommandBus) {}
 
